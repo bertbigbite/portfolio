1647e6b8b3d188478745035bb17cee1897cb1b6b
Michael Bertram <michael@bigbite.net>
2023-10-17 17:46:44 +0100

    Fixed errors within the JS to fetch from the JSON file.

--- script.js
+++ script.js
@@ -3,31 +3,21 @@ fetch('projects.json').then(function (response) {
 }).then(function (projects){
 
     const itemsDiv = document.querySelector ('#name')
-
     let contents = '';
-
-   contents += "<div class='container'>";
-    projects.forEach(projects => {
-        contents += "<div class='project_container'>" + projects.name + "<br>";
-        contents += projects.desc + "<br>";
-        contents += projects.image + "<br>";
-        contents += projects.url + "<br>";
-        contents += projects.git + "<br>";
-        contents += "</div>"
+  contents += "<div class='container'>";
+    projects.forEach(projects =>{
+        contents += `<div class='project_container'>
+            <h1>${projects.name}</h2>
+            <p>${projects.desc}</p>
+            <img>${projects.image}</img>
+            <a>${projects.url}</a>
+            <a>${projects.git}</a>
+        </div>`
+        console.log(contents);
     })
-
-
 contents += "</div>";
 
-    
-
     itemsDiv.innerHTML = contents;
-    
 
 
 })
-
-    
-
-
-

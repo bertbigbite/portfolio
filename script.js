1cc4bda0fb2da0063ac260829677e5d1fda960f6
Michael Bertram <michael@bigbite.net>
2023-10-17 14:46:57 +0100

    Written the javascript code to fetch the .json file

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('test')
+fetch('projects.json').then(function (response) {
+    return response.json()
+}).then(function (projects){
+
+    const itemsDiv = document.querySelector ('#name')
+
+    let contents = '';
+
+   contents += "<div class='container'>";
+    projects.forEach(projects => {
+        contents += "<div class='project_container'>" + projects.name + "<br>";
+        contents += projects.desc + "<br>";
+        contents += projects.image + "<br>";
+        contents += projects.url + "<br>";
+        contents += projects.git + "<br>";
+        contents += "</div>"
+    })
+
+
+contents += "</div>";
+
+    
+
+    itemsDiv.innerHTML = contents;
+    
+
+
+})
+
+    
+
+
+
